4de1ac7d6c14eae2841da1d83a6c34878c899875
Yaro <yshmarov@gmail.com>
2021-03-14 19:32:16 +0000

    gem httparty, send telegram message from controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def group_message
+    HTTParty.post('https://api.telegram.org/bot1601291955:AAHTBAjaqigyYL_5Y70WVMglsnQ_EYPfwDk/sendMessage?chat_id=-572374068&text=hello world')
+
+    redirect_to root_path, notice: "success"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
+  post "home/group_message", to: "home#group_message"
 end
