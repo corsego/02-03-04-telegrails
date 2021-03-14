31e2847049ec3f4f84d07dd4d5c0249a082204e1
Yaro <yshmarov@gmail.com>
2021-03-14 20:00:01 +0000

    improve HTTParty method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,20 @@ class HomeController < ApplicationController
   end
 
   def group_message
-    HTTParty.post('https://api.telegram.org/bot1601291955:AAHTBAjaqigyYL_5Y70WVMglsnQ_EYPfwDk/sendMessage?chat_id=-572374068&text=hello world')
+    api_key = TELEGRAM
+    chat_id = -572374068
+    # chat_id = 450462613
+    text = "I am feeling great today"
+    # HTTParty.post("https://api.telegram.org/bot#{api_key}/sendMessage?chat_id=#{chat_id}&text=#{text}")
+    HTTParty.post("https://api.telegram.org/bot#{api_key}/sendMessage",
+      headers: {
+        'Content-Type' => 'application/json'
+      },
+      body: {
+        chat_id: chat_id,
+        text: text
+      }.to_json
+    )
 
     redirect_to root_path, notice: "success"
   end
