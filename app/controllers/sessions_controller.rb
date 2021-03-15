e21569f306288b1a1da1131908e0e25e374b2dd5
Yaro <yshmarov@gmail.com>
2021-03-15 09:47:57 +0000

    telegram hash to verify authenticity of the params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,17 @@
 class SessionsController < ApplicationController
   
   def create
-    session[:user_id] = params[:id]
-    session[:username] = params[:username]
-    redirect_to root_path, notice: "successful login via telegram"
+    fields = %w[auth_date first_name id last_name photo_url username]
+    secret = OpenSSL::Digest::SHA256.digest(TELEGRAM)
+    signature = fields.filter { |f| params[f] }.map{ |f| "%s=%s" % [f, params[f]] }.join("\n")
+    hashed_signature = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::SHA256.new, secret, signature)
+    if params[:hash] == hashed_signature
+      session[:user_id] = params[:id]
+      session[:username] = params[:username]
+      redirect_to root_path, notice: "successful login via telegram"
+    else
+      redirect_to root_path, notice: "LIAR"
+    end
   end
 
   def destroy
